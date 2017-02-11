1c7a472183ad28f6079ee8e757b13f64fbf45910
computer1 <pakavoor@chapin.edu>
2017-02-11 13:08:28 -0500

    New Scoop/limit switch bug fixes

--- src/org/usfirst/frc/team4856/robot/commands/ReceiveGear.java
+++ src/org/usfirst/frc/team4856/robot/commands/ReceiveGear.java
@@ -26,7 +26,7 @@ public class ReceiveGear extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return Robot.scoop.isSwitchSet();
+    	return Robot.scoop.isSwitch1Set() == true && Robot.scoop.isSwitch2Set() == true;
     }
 
     // Called once after isFinished returns true
