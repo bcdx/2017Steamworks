13ccdf03c47b2a3bec31f71c312272e607f94494
computer1 <pakavoor@chapin.edu>
2017-02-13 18:09:12 -0500

    Added Bucket subsystem

---
+++ src/org/usfirst/frc/team4856/robot/subsystems/Bucket.java
@@ -0,0 +1,25 @@
+package org.usfirst.frc.team4856.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.*;
+
+public class Bucket extends Subsystem {
+
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+	
+	Servo servo;
+	
+	public Bucket(int servo_port) {
+		
+		super();
+		servo = new Servo(servo_port);
+		
+	}
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
